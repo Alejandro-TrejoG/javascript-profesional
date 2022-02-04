5a3b49a8844734c1fac9f44479f1e7ea0928733d
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-02-04 09:03:49 -0600

    Scoope

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,7 @@
-const video  = document.querySelector("video")
-      const button  = document.querySelector("button")
-      function MediaPlayer(config) {
-        this.media = config.el;
-      }
+import MediaPlayer from "./MediaPlayer.js"
 
-      MediaPlayer.prototype.play = function() {
-        this.media.play();
-      };
+const video = document.querySelector("video")
+const button = document.querySelector("button")
 
-      MediaPlayer.prototype.pause = function() {
-        this.media.pause();
-      };
-
-      MediaPlayer.prototype.togglePlay = function(){
-        this.media.paused ? this.play() : this.pause()
-      }
-
-      const player = new MediaPlayer({el: video})
-      button.onclick = () => player.togglePlay()
+const player = new MediaPlayer({ el: video })
+button.onclick = () => player.togglePlay()
