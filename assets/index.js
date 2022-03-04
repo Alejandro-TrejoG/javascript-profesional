5d248432d732474e3866d2aa20e29d55a6c94080
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-03-04 09:26:56 -0600

    ServiceWorker

--- assets/index.js
+++ assets/index.js
@@ -9,3 +9,10 @@ const buttonMute = document.querySelector(".mute")
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay, new AutoPause] })
 buttonPlay.onclick = () => player.togglePlay()
 buttonMute.onclick = () => player.toggleMuted()
+
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").catch(err => {
+        console.log(err)
+    })
+}
---
+++ sw.js
@@ -0,0 +1,44 @@
+self.addEventListener("install", event => {
+    event.waitUntil(precache())
+})
+
+self.addEventListener("fetch", event => {
+    const request = event.request;
+
+    // Evaluamos si el metodo de la peticion es GET
+    if (request.method !== "GET") {
+        return
+    }
+
+    // Buscamos en cache si esta la peticion
+    event.respondWith(cachedResponse(request))
+
+    // Actualizar el cache
+    event.waitUntil(updateCache(request))
+})
+
+async function precache() {
+    const cache = await caches.open("v1")
+    cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/index.js',
+        '/assets/MediaPlayer.js',
+        '/assets/plugins/AutoPlay.js',
+        '/assets/plugins/AutoPause.js',
+        '/assets/index.css',
+        '/assets/BigBuckBunny.mp4',
+    ])
+}
+
+async function cachedResponse(request) {
+    const cache = await caches.open("v1")
+    const response = await cache.match(request)
+    return response || fetch(request)
+}
+
+async function updateCache(request) {
+    const cache = await caches.open("v1")
+    const response = await fetch(request)
+    return cache.put(request, response)
+}
