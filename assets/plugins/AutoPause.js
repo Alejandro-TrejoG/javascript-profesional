ce1c5396e18bd457179e073c5715979f1154f7d2
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-03-03 21:06:43 -0600

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor() {
         this.threshold = 0.25
         this.handlerIntersection = this.handlerIntersection.bind(this)
+        this.handlerVisibility = this.handlerVisibility.bind(this)
     }
     run(player) {
         this.player = player
@@ -10,11 +11,23 @@ class AutoPause {
 
         })
         observer.observe(player.media)
+
+        // VisibilityChange - Nos sirve para saber si el Tab donde estqa nuestra pagina es el que esta observando el usuario,
+        // es decir, es el Tab que esta hasta el frente
+        document.addEventListener("visibilitychange", this.handlerVisibility)
     }
 
     handlerIntersection(entries) {
         const entry = entries[0]
         const isVisible = entry.intersectionRatio >= this.threshold
+        if (isVisible) {
+            this.player.media.play()
+        } else {
+            this.player.media.pause()
+        }
+    }
+    handlerVisibility() {
+        const isVisible = document.visibilityState === "visible"
         if (isVisible) {
             this.player.media.play()
         } else {
