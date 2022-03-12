d627399677eaed011cc6d3d9bf14e87f4082f78f
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-03-12 08:45:42 -0600

    Implementacion del plugin Ads: Desplegando en consola

--- assets/index.ts
+++ assets/index.ts
@@ -1,12 +1,13 @@
 import MediaPlayer from "./MediaPlayer"
 import AutoPlay from "./plugins/AutoPlay"
 import AutoPause from "./plugins/AutoPause"
+import Ads from "./plugins/Ads"
 
 const video = document.querySelector("video")
 const buttonPlay: HTMLElement = document.querySelector(".play-pause")
 const buttonMute: HTMLElement = document.querySelector(".mute")
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay, new AutoPause] })
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause(), new Ads()] })
 buttonPlay.onclick = () => player.togglePlay()
 buttonMute.onclick = () => player.toggleMuted()
 
